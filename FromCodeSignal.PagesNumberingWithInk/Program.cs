c2426619a72f91a17e8229caa7adf2ab6c61fd29
davit.khudaverdyan.96 <davit.khudaverdyan.96@gmail.com>
2019-05-15 23:56:20 +0400

    Solve PagesNumberingWithInk Problem

---
+++ FromCodeSignal.PagesNumberingWithInk/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FromCodeSignal.PagesNumberingWithInk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(pagesNumberingWithInk(21, 5));
+        }
+
+        public static int pagesNumberingWithInk(int current, int numberOfDigits)
+        {
+            while (NumberOfDigits(current) <= numberOfDigits)
+            {
+                numberOfDigits -= NumberOfDigits(current);
+                current++;
+            }
+            return current-1;
+        }
+
+        public static int NumberOfDigits(int number)
+        {
+            return number.ToString().Length;
+        }
+    }
+}
